5a2b41cecc23517cbef64e383be5cdd4f1fd7dec
moitorrijos <juanmtorrijos@gmail.com>
2020-06-20 15:07:10 -0500

    First commit
    Create index php with plugin details and empty style.css

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/***
+ * Plugin Name: Team Member Card Block
+ * Plugin URI: https://github.com/moitorrijos/team-member-card-block
+ * Description: This is a plugin for displaying team member card block
+ * Version: 0.1.0
+ * Author: Juan Moises Torrijos
+ * Author URI: https://moitorrijos.com
+ * 
+ */
